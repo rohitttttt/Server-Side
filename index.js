73b1917a1a03af31cea9c8e1ac94859d1c8f2a91
Rohit Lal <rohitlal1155@gmail.com>
2020-01-23 19:31:04 +0530

    Fixed cors issue

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+var express = require('express')
+var cors = require('cors')
+var app = express()
+
+app.use(cors())
+
+
 var express = require('express'),
   app = express(),
   port = process.env.PORT || 3001,
@@ -12,7 +19,23 @@ var express = require('express'),
   
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/s2s_db'); 
+mongoose.connect('mongodb://localhost/Tododb'); // Rohit Need to Change After some time
+//mongoose.connect('mongodb://localhost/s2s_db');  //Gaurav
+
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  if ('OPTIONS' == req.method) {
+    res.sendStatus(200);
+    } else {
+      next();
+    }
+  //next();
+});
+
+
 
 app.set('secretKey', 'nodeRestApi'); // jwt secret token
 app.use(bodyParser.urlencoded({ extended: false }));
